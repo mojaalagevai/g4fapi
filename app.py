1063798c2a62420f2b8308f31fd9b06fe2968009
mojaalagevai <mojaalagevai@gmail.com>
2025-07-25 02:55:43 +0600

    Update and rename g4f api app.py to app.py

--- app.py
+++ app.py
@@ -189,4 +189,4 @@ async def list_models():
 # Run the server
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=7860)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
